e50d24357ea3418ca6e8ff0fcbc7afdf2c268f15
장진욱 <wfo3977@gmail.com>
2021-02-04 10:38:10 +0900

    productdetailview 프론트와 key값 정리

--- product/views.py
+++ product/views.py
@@ -15,15 +15,14 @@ class ProductDetailView(View):
                 "english_name"      : drink.english_name,
                 "main_description"  : drink.main_description,            
                 "sub_description"   : drink.sub_description,
-                "price"             : drink.price,
                 "img_url"           : drink.image_url,
                 "feel"              : drink.feel,
-                "density"           : drink.density,
                 "taste"             : drink.taste,
                 "size" : [{
                     "size_id"     : nutrition.size.id,
                     "size_name"   : nutrition.size.name,
-                    "nutrtitions" : {
+                    "price"       : int(drink.price) if nutrition.size.name == "Tall(톨)" else int(drink.price) * 1.5,
+                    "nutritions" : {
                         "kcal"       : nutrition.kcal,
                         "sodium"     : nutrition.sodium,
                         "saturation" : nutrition.saturation,
@@ -38,7 +37,7 @@ class ProductDetailView(View):
                     "allergy_name" : allergy.name,
                 } for allergy in drink.allergies.all()]
             }
-            return JsonResponse({'product' : product_info}, status = 200)
+            return JsonResponse({'item' : [product_info]}, status = 200)
 
         except Drink.DoesNotExist:
             return JsonResponse({'error' : "DRINK_DOES_NOT_EXIST"}, status=400)
